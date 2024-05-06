e357737d07e454b1c303f2a4c3ee52638a832f2a
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-06 10:29:16 +0530

    hero-section done

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overFlowHidden");
+    document.querySelector(".menuIcon").classList.toggle("cross")
+}
+// ============dropdown
+function toggleClass() {
+    const btnn = document.getElementById('btnn');
+    btnn.classList.toggle("active");
+}
